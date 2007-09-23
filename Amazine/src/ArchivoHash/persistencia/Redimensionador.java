4008dc1ed9f3da69d54717658dc7aac5c08b14f8
apoteoticos@gmail.com <apoteoticos@gmail.com@019ee545-ce3a-0410-999c-a994cd6c74f2>
2007-09-23 15:11:31 +0000

    archivos hash

--- Amazine/src/ArchivoHash/persistencia/Redimensionador.java
+++ Amazine/src/ArchivoHash/persistencia/Redimensionador.java
@@ -1,34 +1,31 @@
-package persistencia;
+package ArchivoHash.persistencia;
+
 /**
- * Hilo que correr· paralelo a la ejecuciÛn del programa a fines de controlar
+ * Hilo que correr√° paralelo a la ejecuci√≥n del programa a fines de controlar
  * que el archivo hash siga siendo eficiente, redimensionando en caso de ser necesario.
  * @author Fernando Cargnelutti.
  */
 public class Redimensionador extends Thread {
-	
-	private ArchivoHash archivo;
-	public Redimensionador(ArchivoHash ar)
-	{
-		archivo=ar;
-	}
-	
-	public void run()
-	{
-		while(true)
-		{
-			try{					
-					sleep(5000);
-					if(archivo.isOvercharged())
-					{					
-						long nuevaCapacidad=archivo.getCapacity()*2; // el nuevo tamaÒo ser· un 100% m·s grande que el original
-					    archivo.redimensionar(nuevaCapacidad);					       
-					}				
-			}catch(InterruptedException ie)
-			{
-				System.out.println("El hilo est· interrumpido.");
-				interrupted();
-				break;
-			}
+
+    private ArchivoHash archivo;
+
+    public Redimensionador(ArchivoHash ar) {
+	archivo = ar;
+    }
+
+    public void run() {
+	while (true) {
+	    try {
+		sleep(5000);
+		if (archivo.isOvercharged()) {
+		    long nuevaCapacidad = archivo.getCapacity() * 2; // el nuevo tama√±o ser√° un 100% m√°s grande que el original
+		    archivo.redimensionar(nuevaCapacidad);
 		}
+	    } catch (InterruptedException ie) {
+		System.out.println("El hilo est√° interrumpido.");
+		interrupted();
+		break;
+	    }
 	}
+    }
 }
