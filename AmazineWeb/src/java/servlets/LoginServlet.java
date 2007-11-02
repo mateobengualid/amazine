bca1f9c54c4a23cd88b2d36d654e285a070b7bcc
apoteoticos@gmail.com <apoteoticos@gmail.com@019ee545-ce3a-0410-999c-a994cd6c74f2>
2007-11-02 19:09:20 +0000

    Amazine Web

--- AmazineWeb/src/java/servlets/LoginServlet.java
+++ AmazineWeb/src/java/servlets/LoginServlet.java
@@ -10,11 +10,12 @@
 package servlets;
 
 import com.DBManager;
-import java.io.*;
-import java.net.*;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.ResultSet;
+import java.util.Hashtable;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import javax.naming.InitialContext;
 import javax.servlet.*;
 import javax.servlet.http.*;
 
@@ -32,28 +33,45 @@ public class LoginServlet extends HttpServlet
     protected void processRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
     {
 	RequestDispatcher disp;
-	ServletContext app = this.getServletContext();	
+	ServletContext app = this.getServletContext();
 
 	response.setContentType("text/html;charset=UTF-8");
 	PrintWriter out = response.getWriter();
-	
+
 	String destination;
-	
+
 	try
 	{
 	    DBManager dbmanager = new DBManager();
-	    dbmanager.execute("Select");
-	    destination = "index.jsp";
+	    Hashtable<String, String> hs = new Hashtable<String, String>();
+	    /*hs.put("login", request.getParameter("user"));
+	    hs.put("password", request.getParameter("password"));*/
+
+	    ResultSet rs = dbmanager.openPreparedQuery("select * " +
+						       "from usuario " +
+						       "where login = '" +
+						       request.getParameter("user") +
+						       "' and password = '" +
+						       request.getParameter("password")+"'", hs);
+
+	    if (!rs.first())
+	    {
+		throw new SecurityException("Acceso denegado, nombre de usuario " +
+					    "y contraseña no validos");
+	    }
+
+	    destination = "/index.jsp";
 	}
 	catch (Exception ex)
 	{
+	    ex.printStackTrace();
 	    request.getSession().setAttribute("Error", ex.getMessage());
-	    destination = "error.jsp";
+	    destination = "/error.jsp";
 	    Logger.getLogger(LoginServlet.class.getName()).log(Level.SEVERE, null, ex);
 	}
-	
+
 	disp = app.getRequestDispatcher(destination);
-	disp.forward(request,response);
+	disp.forward(request, response);
     }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
